3edea16c5184be965beae5e028b4cdaec6aa9ebb
umacdevi <59162303+umacdevi@users.noreply.github.com>
2026-04-02 20:06:28 +0530

    Add depthwise convolution operation constant

--- torch_spyre/_inductor/constants.py
+++ torch_spyre/_inductor/constants.py
@@ -16,6 +16,7 @@ BATCH_MATMUL_OP = "batchmatmul"
 CLONE_OP = "clone"
 MATMUL_REDUCTION_OP = "matmul"
 TRANSPOSE_OP = "transpose"
+DEPTHWISE_CONV2D_OP = "depthwiseconv2dnative"
 
 DEVICE_NAME = "spyre"
 
